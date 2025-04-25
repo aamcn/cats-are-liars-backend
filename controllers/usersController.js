bca7b7d745422100caa622378c5dcfe8e102d04a
malcomcrab <malcomcrabesq@outlook.com>
2025-04-25 11:51:10 +0100

    Feat: Add users Router, controller and database queries

---
+++ controllers/usersController.js
@@ -0,0 +1,31 @@
+const bcrypt = require("bcryptjs")
+const queries = require("../db/usersQueries.js")
+
+
+
+async function createNewUser(req, res) {
+    const username = req.body.username 
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    
+    try{
+        await queries.addUser(username, hashedPassword)
+        res.send('yay')
+    } catch(err) {
+        return next(err)
+    }
+}
+
+async function allUsernames(req, res) {
+    const usernames =  await queries.getAllUsernames();
+    console.log(usernames.rows)
+    res.send(usernames.rows)
+ } 
+ 
+async function getUserById(req, res) {
+    const id = req.body.id
+    const user = await queries.userById(id);
+    console.log(user.rows)
+    res.send(user.rows)
+}
+
+module.exports = { allUsernames, createNewUser, getUserById}
---
+++ routes/usersRouter.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const { allUsernames, createNewUser, getUserById} = require("../controllers/usersController")
+const usersRouter = Router();
+
+
+usersRouter.post("/sign-up", createNewUser);
+usersRouter.get("/usernames", allUsernames);
+usersRouter.get("/user-by-id", getUserById)
+
+module.exports = usersRouter;  
