854ce58970acc3402265e61797f4a191d21bcf6e
malcomcrab <malcomcrabesq@outlook.com>
2025-04-25 11:32:42 +0100

    Add pool.js with connection information

--- app.js
+++ app.js
@@ -21,4 +21,8 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.render("index"));
 
+
+
+
+
 app.listen(3000, () => console.log("app listening on port 3000!"));
---
+++ db/pool.js
@@ -0,0 +1,12 @@
+
+
+
+const { Pool } = require("pg");
+
+module.exports = new Pool({
+    host: process.env.POOLHOST,
+    user: process.env.POOLUSER,
+    database: process.env.POOLDB,
+    password: process.env.POOLPASSWORD,
+    port: process.env.POOLPORT
+});
