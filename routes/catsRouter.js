a26e83eb60da8778a8a2d018c0b0bf8ca17ebc72
malcomcrab <malcomcrabesq@outlook.com>
2025-04-28 18:24:04 +0100

    Perf: Refactor userController to import functions in a less cluttered method

--- routes/catsRouter.js
+++ routes/catsRouter.js
@@ -6,6 +6,7 @@ const verifyToken = require("../verifyToken")
 catsRouter.get("/", verifyToken.verifyToken, catsController.getAllCats);
 catsRouter.get("/cat-name/:name", verifyToken.verifyToken, catsController.getCatByName);
 catsRouter.get("/cat-id/:catId", verifyToken.verifyToken, catsController.getCatByCatId);
+
 catsRouter.post("/add-new-cat", verifyToken.verifyToken, catsController.addCatToTable);
 catsRouter.post("/cat-id/:catId/update/", verifyToken.verifyToken, catsController.updateCatDetails);
 catsRouter.post("/add-feeder", verifyToken.verifyToken, catsController.addCatFeederById);
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,17 +1,11 @@
 const { Router } = require("express");
 const verifyToken = require("../verifyToken")
-
-
-const {
-  allUsernames,
-  createNewUser,
-  getUserById,
-} = require("../controllers/usersController");
+const usersController = require("../controllers/usersController");
 const usersRouter = Router();
 
-usersRouter.post("/sign-up", createNewUser);
-usersRouter.get("/usernames",  verifyToken.verifyToken,  allUsernames);
-usersRouter.get("/user-by-id", verifyToken.verifyToken,  getUserById);
+usersRouter.post("/sign-up", usersController.createNewUser);
+usersRouter.get("/usernames",  verifyToken.verifyToken,  usersController.allUsernames);
+usersRouter.get("/user-by-id", verifyToken.verifyToken,  usersController.getUserById);
 
 module.exports = usersRouter;
 
