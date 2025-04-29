90edaa0bfa5bd7f27e0c999bd3d40c6964b38078
malcomcrab <malcomcrabesq@outlook.com>
2025-04-29 18:26:15 +0100

    Bug: Remove bug causing req.user to be overwritten during successRedirect

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
 require("dotenv").config();
-const jwt = require('jsonwebtoken');
 const express = require("express");
+const bodyParser = require('body-parser')
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-const usersRouter = require("./routes/usersRouter");
-const catsRouter = require("./routes/catsRouter");
-const feedHistoryRouter = require("./routes/feedHistoryRouter");
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const pool = require("./db/pool");
 const { bcrypt, compare } = require("bcryptjs")
 const verifyToken = require("./verifyToken")
+
+const usersRouter = require("./routes/usersRouter");
+const catsRouter = require("./routes/catsRouter");
+const feedHistoryRouter = require("./routes/feedHistoryRouter");
 const app = express();
 
-app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(cors())
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false,  cookie: { secure: true }}));
+app.use(passport.initialize());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(passport.authenticate('session'))
+
+app.use(cookieParser());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: true }));
 app.use("/users", usersRouter);
 app.use("/cats", catsRouter);
 app.use("/feed-history", feedHistoryRouter);
-
-
 //Finds username in the 'users' table and checks if the entered username and password match. 
 passport.use(
   new LocalStrategy(async (username, password, done) => {
+    
     try {
+      
       const { rows } = await pool.query(
         "SELECT * FROM users WHERE username = $1",
         [username],
@@ -35,16 +47,18 @@ passport.use(
       }
       //If user is found, the entered password is hashed and compared to the stored password.
       const match = await compare(password, user.password);
+      console.log(match)
       //If they do not match a password error is returned.
       if (!match) {
         return done(null, false, { message: "Incorrect password" });
       }
       //If username and password match the log in is succesful and the user object is returned.
+      
       return done(null, user);
-    } catch (err) {
+    } catch(err) {
       return done(err);
     }
-  }),
+  })
 );
 
 // Takes a callback containing information to be stored in session data.
@@ -56,14 +70,13 @@ passport.serializeUser((user, done) => {
 Is called when retrieving a session, to extract serialized data to attach something to the .user property of req.user
 to be used in the rest of the request.
 */
-passport.deserializeUser(async (id, done) => {
+passport.deserializeUser(async (id, done) => { 
+  
   try {
-    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
-      id,
-    ]);
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = ($1)",[id]);
     const user = rows[0];
     done(null, user);
-  } catch (err) {
+  } catch(err) {
     done(err);
   }
 });
@@ -97,29 +110,25 @@ app.post('/posts', verifyToken.verifyToken, (req, res) => {
 When user click enter on the log in form the entered password and username are authenticated using passport. 
 depending on succesful or unsuccesful log in the browser redirects to the assigned url. 
 */
-app.post("/users/log-in",
-  passport.authenticate("local", {
-    successRedirect: "/login-success",
-    failureRedirect: "/login-fail",
-  }),
-);
-
-/* 
-When a log in is succesful, a unique 'json web token' is created and returned to the browser to be stored and used for
- verifying database queries. 
- */
-app.get("/login-success", (req, res) => {
-  const user = req.user
+app.post("/log-in",
+  passport.authenticate("local",  { failureRedirect: '/login-fail', failureMessage: true }),
+  function(req, res) {
+    const user = req.user
+    const userId = req.user.id
   // json web tokens currently expire after 30 minutes and the user must log in again to receive a new token to make database queries.
-  jwt.sign({user: user}, 'secretkey', {expiresIn: '30m'}, (err,token) => {
+  jwt.sign({user}, 'secretkey', {expiresIn: '30m'}, (err,token) => {
     res.json({
-      token
+      token, 
+      userId
     })
   })
-});
+  }
+);
+
 
 
 app.get("/login-fail", (req, res) => {
+  console.log(req.body)
   res.send("fail");
 });
 
