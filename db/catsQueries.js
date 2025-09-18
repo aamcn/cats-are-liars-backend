8981133a99801fe8ec64e272aa363675b34b53d1
aaronmcnulty <malcomcrabesq@outlook.com>
2025-09-18 15:29:21 +0100

    Refactor: Add error handling and error messages to feedHistoryQueries.js functions

--- db/catsQueries.js
+++ db/catsQueries.js
@@ -2,11 +2,11 @@ const pool = require("./pool");
 
 // Returns all rows from the 'cats' table that contain a feeder_userid that matches the passed in argument
 async function allCats(userId) {
-
   try {
-    return await pool.query("SELECT * FROM cats WHERE  ($1)=ANY(feeder_userid)", [
-      userId,
-    ]);
+    return await pool.query(
+      "SELECT * FROM cats WHERE  ($1)=ANY(feeder_userid)",
+      [userId]
+    );
   } catch (error) {
     console.error("Error fetching all cats:", error);
     throw error;
@@ -18,7 +18,7 @@ async function catByName(catName, userId) {
   try {
     return await pool.query(
       "SELECT * FROM cats WHERE name = ($1) AND ($2)=ANY(feeder_userid)",
-      [catName, userId],
+      [catName, userId]
     );
   } catch (error) {
     console.error("Error fetching cat by name:", error);
@@ -31,7 +31,7 @@ async function catByCatId(catId, userId) {
   try {
     return await pool.query(
       "SELECT * FROM cats WHERE catId = ($1) AND ($2)=ANY(feeder_userid)",
-      [catId, userId],
+      [catId, userId]
     );
   } catch (error) {
     console.error("Error fetching cat by ID:", error);
@@ -42,9 +42,9 @@ async function catByCatId(catId, userId) {
 // Inserts a new row into the 'cats' table and populates it's columns with the passed in arguments.
 async function addCatToTable(name, meals, medication, userId) {
   try {
-      await pool.query(
+    await pool.query(
       "INSERT INTO cats (name, meals, medication, feeder_userid) VALUES ($1, $2, $3, $4)",
-      [name, meals, medication, [userId]],
+      [name, meals, medication, [userId]]
     );
   } catch (error) {
     console.error("Error adding cat to database:", error);
@@ -55,9 +55,9 @@ async function addCatToTable(name, meals, medication, userId) {
 // Updates an existing 'cats' row with the passed in arguments where the feeder_userid matches the 'userId' argument.
 async function updateCat(catName, meals, medication, catid, userId) {
   try {
-     await pool.query(
+    await pool.query(
       "UPDATE cats SET name = ($1), meals = ($2), medication = ($3) WHERE catid = ($4) AND ($5)=ANY(feeder_userid)",
-      [catName, meals, medication, catid, userId],
+      [catName, meals, medication, catid, userId]
     );
   } catch (error) {
     console.error("Error updating cat:", error);
@@ -74,7 +74,7 @@ async function addCatFeeder(newFeederId, catId, userId) {
   try {
     await pool.query(
       "UPDATE cats SET feeder_userid = feeder_userid || ($1) WHERE catid = ($2) AND ($3)=ANY(feeder_userid)",
-      [[newFeederId], catId, userId],
+      [[newFeederId], catId, userId]
     );
   } catch (error) {
     console.error("Error adding cat feeder:", error);
--- db/feedHistoryQueries.js
+++ db/feedHistoryQueries.js
@@ -2,72 +2,117 @@ const pool = require("./pool");
 
 // Returns all rows from the feeding_history database table.
 async function allEntries(userId) {
-  return await pool.query(
-    "SELECT * FROM feeding_history WHERE ($1)=ANY(authorised_feeders);",
-    [userId],
-  );
+  try {
+    return await pool.query(
+      "SELECT * FROM feeding_history WHERE ($1)=ANY(authorised_feeders);",
+      [userId]
+    );
+  } catch (error) {
+    console.error("Error fetching all feeding history entries:", error);
+    throw error;
+  }
 }
 
 async function allEntriesBetweenDates(fromDate, toDate, userId) {
-  console.log(fromDate, toDate)
-  return await pool.query(
-    `SELECT * FROM feeding_history WHERE date >= ($1) AND date < ($2) AND '${userId}'=ANY(authorised_feeders)`,
-    [fromDate, toDate],
-  );
+  try {
+    return await pool.query(
+      `SELECT * FROM feeding_history WHERE date >= ($1) AND date < ($2) AND '${userId}'=ANY(authorised_feeders)`,
+      [fromDate, toDate]
+    );
+  } catch (error) {
+    console.error(
+      "Error fetching feeding history entries between dates:",
+      error
+    );
+    throw error;
+  }
 }
 
 async function allByMonthYear(month, year, userId) {
-  return await pool.query(
-    `SELECT * FROM feeding_history WHERE date::text LIKE '${year}-${month}%' AND '${userId}'=ANY(authorised_feeders);`,
-  );
+  try {
+    return await pool.query(
+      `SELECT * FROM feeding_history WHERE date::text LIKE '${year}-${month}%' AND '${userId}'=ANY(authorised_feeders);`
+    );
+  } catch (error) {
+    console.error(
+      "Error fetching feeding history entries by month and year:",
+      error
+    );
+    throw error;
+  }
 }
 
 async function allByYear(year, userId) {
-  return await pool.query(
-    `SELECT * FROM feeding_history WHERE date::text LIKE '${year}%' AND '${userId}'=ANY(authorised_feeders);`,
-  );
+  try {
+    return await pool.query(
+      `SELECT * FROM feeding_history WHERE date::text LIKE '${year}%' AND '${userId}'=ANY(authorised_feeders);`
+    );
+  } catch (error) {
+    console.error("Error fetching feeding history entries by year:", error);
+    throw error;
+  }
 }
 
 // Finds and returns the row where the id matches the 'entryId' argument.
 async function entryById(entryId, userId) {
-  return await pool.query(
-    "SELECT * FROM feeding_history WHERE id = ($1) AND ($2)=ANY(authorised_feeders)",
-    [entryId, userId],
-  );
+  try {
+    return await pool.query(
+      "SELECT * FROM feeding_history WHERE id = ($1) AND ($2)=ANY(authorised_feeders)",
+      [entryId, userId]
+    );
+  } catch (error) {
+    console.error("Error fetching feeding history entry by ID:", error);
+    throw error;
+  }
 }
 
 // Finds and returns all entries where the cat_name matches the 'catName' argument.
 async function entriesByCatName(catName, userId) {
-  return await pool.query(
-    "SELECT * FROM feeding_history WHERE cat_name = ($1) AND ($2)=ANY(authorised_feeders)",
-    [catName, userId],
-  );
+  try {
+    return await pool.query(
+      "SELECT * FROM feeding_history WHERE cat_name = ($1) AND ($2)=ANY(authorised_feeders)",
+      [catName, userId]
+    );
+  } catch (error) {
+    console.error("Error fetching feeding history entries by cat name:", error);
+    throw error;
+  }
 }
 
 // Inserts a new row(entry) into the table with the passed in arguments as its values.
 async function insertFeedingEntry(entry, user) {
-  await pool.query(
-    "INSERT INTO feeding_history (catid, cat_name, feeder_id, feeder_username, medication_needed, medication_given, time, notes, authorised_feeders) values ($1, $2, $3, $4, $5, $6, $7, $8, $9)",
-    [
-      entry.catid,
-      entry.cat_name,
-      user.id,
-      entry.feeder_username,
-      entry.medication_needed,
-      entry.medication_given,
-      entry.time,
-      entry.notes,
-      entry.authorised_feeders.split(","),
-    ],
-  );
+  try {
+    await pool.query(
+      "INSERT INTO feeding_history (catid, cat_name, feeder_id, feeder_username, medication_needed, medication_given, time, notes, authorised_feeders) values ($1, $2, $3, $4, $5, $6, $7, $8, $9)",
+      [
+        entry.catid,
+        entry.cat_name,
+        user.id,
+        entry.feeder_username,
+        entry.medication_needed,
+        entry.medication_given,
+        entry.time,
+        entry.notes,
+        entry.authorised_feeders.split(","),
+      ]
+    );
+  } catch (error) {
+    console.error("Error adding feeding history entry:", error);
+    throw error;
+  }
 }
 
-// Finds and deletes the tablel row where the id matches the 'entryId' argument.
+// Finds and deletes the table row where the id matches the 'entryId' argument.
 async function deleteEntryById(entryId, userId) {
-  await pool.query(
-    "DELETE FROM feeding_history WHERE id = ($1) AND feeder_id = ($2)",
-    [entryId, userId],
-  );
+  try {
+    await pool.query(
+      "DELETE FROM feeding_history WHERE id = ($1) AND feeder_id = ($2)",
+      [entryId, userId]
+    );
+  } catch (error) {
+    console.error("Error deleting feeding history entry by id:", error);
+    throw error;
+  }
 }
 
 module.exports = {
