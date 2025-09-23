0cbc538f6bc9b6fe5602d2a4481ea67a22a05e04
aaronmcnulty <malcomcrabesq@outlook.com>
2025-09-23 13:34:07 +0100

    Refactor: memoryRouter to use JwtCheck for token validation

--- routes/messagesRouter.js
+++ routes/messagesRouter.js
@@ -1,15 +1,16 @@
 const express = require("express");
-
-
 const messagesController = require("../controllers/messagesController.js");
-const { validateAccessToken } = require("../middleware/auth0.middleware.js");
-
+const { jwtCheck } = require("../verify/jwtCheck.js");
 const messagesRouter = express.Router();
 
 messagesRouter.get("/public", messagesController.getPublicMessage);
 
-messagesRouter.get("/protected", validateAccessToken, messagesController.getProtectedMessage);
+messagesRouter.get(
+  "/protected",
+  jwtCheck,
+  messagesController.getProtectedMessage,
+);
 
-messagesRouter.get("/admin", validateAccessToken, messagesController.getAdminMessage);
+messagesRouter.get("/admin", jwtCheck, messagesController.getAdminMessage);
 
-module.exports =  messagesRouter ;
+module.exports = messagesRouter;
